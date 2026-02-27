5df4d719279633223c7a500ac8f47a5aa15c58b6
André Oliveira <andrelitoscria@gmail.com>
2026-02-26 21:17:34 -0300

    .

--- backend/app.js
+++ backend/app.js
@@ -56,51 +56,129 @@ app.get("/", (req, res) => {
     });
 });
 
+// Rota Principal contendo a situação
+app.get("/:situacao", (req, res) => {
+    // SQL
+    let sql = "SELECT * FROM produtos";
+    // Executa no banco
+    conexao.query(sql, (erro, resultado) => {
+        if (erro) throw erro;
+        res.render("formulario", { produtos: resultado, situacao: req.params.situacao });
+    });
+});
+
 // Rota de cadastro enxuta!
 app.post("/cadastrar", (req, res) => {
-    // 1. Pega os dados
+   try{
+     // 1. Pega os dados
     let nome = req.body.nome;
-    let valor = req.body.valor; 
-    let imagem = req.files.imagem.name; 
+    let valor = req.body.valor;
+    let imagem = req.files.imagem.name;
 
-    // 2. Monta o SQL
-    let sql = "INSERT INTO produtos (nome, valor, imagem) VALUES (?, ?, ?)";
-    
-    // 3. Executa no banco e salva a foto
-    conexao.query(sql, [nome, valor, imagem], (erro, resultado) => {
-        if (erro) throw erro;
-        
-        // Move a foto para a pasta images
-        req.files.imagem.mv(__dirname + '/images/' + imagem); 
-        
-        console.log("Salvo com sucesso! ID do produto:", resultado.insertId);
-        
-        // 4. A RESPOSTA FINAL (Fica aqui dentro!)
-        // Como você tinha colocado um redirect, vou manter ele como padrão.
-        // Assim, quando o cara cadastrar, a tela pisca e volta pro formulário limpo.
-        res.redirect("/"); 
+    // validar o nome do produto e o valor
+    if(nome == '' || valor == '' || isNaN(valor)){
+        res.redirect("/falhaCadastro");
+    }else{
+        // 2. Monta o SQL
+        let sql = "INSERT INTO produtos (nome, valor, imagem) VALUES (?, ?, ?)";
+
+        // 3. Executa no banco e salva a foto
+        conexao.query(sql, [nome, valor, imagem], (erro, resultado) => {
+            if (erro) throw erro;
+
+            // Move a foto para a pasta images
+            req.files.imagem.mv(__dirname + '/images/' + imagem);
+
+            console.log("Salvo com sucesso! ID do produto:", resultado.insertId);
+
+            // 4. A RESPOSTA FINAL (Fica aqui dentro!)
+            // Como você tinha colocado um redirect, vou manter ele como padrão.
+            // Assim, quando o cara cadastrar, a tela pisca e volta pro formulário limpo.
+            res.redirect("/okCadastro");
     });
+    }
+   }catch(erro){
+    res.redirect("/falhaCadastro");
+   }
 });
 
 // rota de delete
 app.get("/deletar/:codigo&:imagem", (req, res) => {
-   let sql = `DELETE FROM produtos WHERE codigo = ${req.params.codigo}`;
+    
+    // Tratamento de exeção
+    try{
+        let sql = `DELETE FROM produtos WHERE codigo = ${req.params.codigo}`;
 
-   conexao.query(sql, (erro, resultado) => {
-       if (erro) throw erro;
-       fs.unlink(__dirname + '/images/' + req.params.imagem, (erro_imagem) => {
-            if (erro_imagem) console.log("Imagem deu ERRO!");
-       });
-       
-       res.redirect("/"); 
-   });
+        conexao.query(sql, (erro, resultado) => {
+            if (erro) throw erro;
+            fs.unlink(__dirname + '/images/' + req.params.imagem, (erro_imagem) => {
+                if (erro_imagem) console.log("Imagem deu ERRO!");
+            });
+        });
+    res.redirect("/okDelete");
+    }catch(erro){
+        res.redirect("/falhaDelete");
+    }
+
 });
 
 // Rota para redirecionar para o formulário de edição
 app.get("/formularioEditar/:codigo", (req, res) => {
-    res.render("formularioEditar");
+    // sql
+    let sql = `SELECT * FROM produtos WHERE codigo = ${req.params.codigo}`;
+    // executar comando sql 
+    conexao.query(sql, (erro, retorno) => {
+        // Caso haja falha no comando
+        if (erro) throw erro;
+        // Caso funcione
+        res.render("formularioEditar", { produto: retorno[0] });
+    })
 });
 
+// Rota para editar produtos
+app.post("/editar", (req, res) => {
+    // obter dados
+    let codigo = req.body.codigo;
+    let nome = req.body.nome;
+    let valor = req.body.valor;
+    let nomeImagem = req.body.nomeImagem;
+
+    // validar o nome do produto e o valor
+    if(nome == '' || valor == '' || isNaN(valor)){
+        res.redirect("/falhaEdicao");
+    } else {
+
+    // definir o tipo de edição
+    try {
+        // objeto de imagem
+        let imagem = req.files.imagem;
+        // sql
+        let sql = `UPDATE produtos SET nome = '${nome}', valor = ${valor}, imagem = '${imagem.name}' WHERE codigo = ${codigo}`;
+        // executar comando sql 
+        conexao.query(sql, (erro, resultado) => {
+            if (erro) throw erro;
+            // Remover imagem antiga
+            fs.unlink(__dirname + '/images/' + nomeImagem, (erro_imagem) => {
+                if (erro_imagem) console.log("Imagem deu ERRO!");
+            })
+            // cadastrar nova imagem
+            imagem.mv(__dirname + '/images/' + imagem.name);
+        });
+
+
+    } catch (erro) {
+        // sql
+        let sql = `UPDATE produtos SET nome = '${nome}', valor = ${valor} WHERE codigo = ${codigo}`;
+        // executar comando sql 
+        conexao.query(sql, (erro, resultado) => {
+            // caso funcione
+            if (erro) throw erro;
+        });
+    }
+    // Finalizar rota
+    res.redirect("/okEdicao");
+    }
+});
 // Iniciar servidor
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
