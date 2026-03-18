c6c376f62d732fd4b57a93178c849fff86ac0aa7
André Oliveira <andrelitoscria@gmail.com>
2026-03-18 09:16:43 -0300

    .

--- backend/servicos/produtos_servico.js
+++ backend/servicos/produtos_servico.js
@@ -5,19 +5,19 @@ const conexao = require('../bd/conexao_mysql');
 const fs = require('fs');
 
 // Função para exibir o formulário para cadastro de produtos
-function formularioCadastro(req, res){
+function formularioCadastro(req, res) {
     res.render("formulario");
 }
 
 // Função para exibir o formulário para cadastro de produtos e a situação
-function formularioCadastroComSituacao(req, res){
+function formularioCadastroComSituacao(req, res) {
     res.render("formulario", { situacao: req.params.situacao });
 }
 
 // Função para exibir o formulário para edição de produtos
-function formularioEditar(req, res){
+function formularioEditar(req, res) {
     // sql
-    let sql = `SELECT * FROM produtos WHERE codigo = ${req.params.codigo}`;
+    let sql = `SELECT * FROM produtos WHERE codigo = '${req.params.codigo}'`;
     // executar comando sql 
     conexao.query(sql, (erro, retorno) => {
         // Caso haja falha no comando
@@ -28,14 +28,14 @@ function formularioEditar(req, res){
 }
 
 // Função para exibir a listagem de produtos
-function listagemProdutos(req, res){
+function listagemProdutos(req, res) {
     let categoria = req.params.categoria;
     let sql = "";
-    
-    if(categoria == "todos"){
-        sql = "SELECT * FROM produtos";
+
+    if (categoria == "todos") {
+        sql = "SELECT * FROM produtos ORDER BY RAND()";
     } else {
-        sql = `SELECT * FROM produtos WHERE categoria = '${categoria}'`;
+        sql = `SELECT * FROM produtos WHERE categoria = '${categoria}' ORDER BY codigo DESC`;
     }
 
     conexao.query(sql, (erro, resultado) => {
@@ -45,7 +45,7 @@ function listagemProdutos(req, res){
 }
 
 // Função para realizar a pesquisa de produtos
-function pesquisa(req, res){
+function pesquisa(req, res) {
     // Obter o termo pesquisado
     let termo = req.body.termo;
     // Sql
@@ -54,51 +54,51 @@ function pesquisa(req, res){
     conexao.query(sql, (erro, resultado) => {
         let semResgistros = resultado.length == 0 ? true : false;
 
-        res.render("lista", { produtos: resultado, semResgistros: semResgistros});
+        res.render("lista", { produtos: resultado, semResgistros: semResgistros });
     });
 }
 
 // Função para realizar o cadastro de produtos
-function cadastrarProduto(req, res){
-    try{
-     // 1. Pega os dados
-    let nome = req.body.nome;
-    let valor = req.body.valor;
-    let categoria = req.body.categoria;
-    let imagem = req.files.imagem.name;
+function cadastrarProduto(req, res) {
+    try {
+        // 1. Pega os dados
+        let nome = req.body.nome;
+        let valor = req.body.valor;
+        let categoria = req.body.categoria;
+        let imagem = req.files.imagem.name;
 
-    // validar o nome do produto e o valor
-    if(nome == '' || valor == '' || isNaN(valor) || categoria == ''){
+        // validar o nome do produto e o valor
+        if (nome == '' || valor == '' || isNaN(valor) || categoria == '') {
+            res.redirect("/falhaCadastro");
+        } else {
+            // 2. Monta o SQL
+            let sql = `INSERT INTO produtos (nome, valor, categoria, imagem) VALUES ('${nome}', ${valor}, '${categoria}', '${imagem}')`;
+
+            // 3. Executa no banco e salva a foto
+            conexao.query(sql, (erro, resultado) => {
+                if (erro) throw erro;
+
+                // Move a foto para a pasta images
+                req.files.imagem.mv(__dirname + '/images/' + imagem);
+
+                console.log("Salvo com sucesso! ID do produto:", resultado.insertId);
+
+                // 4. A RESPOSTA FINAL (Fica aqui dentro!)
+                // Como você tinha colocado um redirect, vou manter ele como padrão.
+                // Assim, quando o cara cadastrar, a tela pisca e volta pro formulário limpo.
+                res.redirect("/okCadastro");
+            });
+        }
+    } catch (erro) {
         res.redirect("/falhaCadastro");
-    }else{
-        // 2. Monta o SQL
-        let sql = `INSERT INTO produtos (nome, valor, categoria, imagem) VALUES ('${nome}', ${valor}, '${categoria}', '${imagem}')`;
-
-        // 3. Executa no banco e salva a foto
-        conexao.query(sql, (erro, resultado) => {
-            if (erro) throw erro;
-
-            // Move a foto para a pasta images
-            req.files.imagem.mv(__dirname + '/images/' + imagem);
-
-            console.log("Salvo com sucesso! ID do produto:", resultado.insertId);
-
-            // 4. A RESPOSTA FINAL (Fica aqui dentro!)
-            // Como você tinha colocado um redirect, vou manter ele como padrão.
-            // Assim, quando o cara cadastrar, a tela pisca e volta pro formulário limpo.
-            res.redirect("/okCadastro");
-    });
     }
-   }catch(erro){
-    res.redirect("/falhaCadastro");
-   }
 }
 
 // Função para realizar a remoção de produtos
-function removerProduto(req, res){
-     // Tratamento de exeção
-    try{
-        let sql = `DELETE FROM produtos WHERE codigo = ${req.params.codigo}`;
+function removerProduto(req, res) {
+    // Tratamento de exeção
+    try {
+        let sql = `DELETE FROM produtos WHERE codigo = '${req.params.codigo}'`;
 
         conexao.query(sql, (erro, resultado) => {
             if (erro) throw erro;
@@ -106,14 +106,14 @@ function removerProduto(req, res){
                 if (erro_imagem) console.log("Imagem deu ERRO!");
             });
         });
-    res.redirect("/okDelete");
-    }catch(erro){
+        res.redirect("/okDelete");
+    } catch (erro) {
         res.redirect("/falhaDelete");
     }
 }
 
 // Função responsável pela edição de produtos
-function editarProduto(req, res){
+function editarProduto(req, res) {
     // obter dados
     let codigo = req.body.codigo;
     let nome = req.body.nome;
@@ -121,39 +121,39 @@ function editarProduto(req, res){
     let nomeImagem = req.body.nomeImagem;
 
     // validar o nome do produto e o valor
-    if(nome == '' || valor == '' || isNaN(valor)){
+    if (nome == '' || valor == '' || isNaN(valor)) {
         res.redirect("/falhaEdicao");
     } else {
 
-    // definir o tipo de edição
-    try {
-        // objeto de imagem
-        let imagem = req.files.imagem;
-        // sql
-        let sql = `UPDATE produtos SET nome = '${nome}', valor = ${valor}, imagem = '${imagem.name}' WHERE codigo = ${codigo}`;
-        // executar comando sql 
-        conexao.query(sql, (erro, resultado) => {
-            if (erro) throw erro;
-            // Remover imagem antiga
-            fs.unlink(__dirname + '/images/' + nomeImagem, (erro_imagem) => {
-                if (erro_imagem) console.log("Imagem deu ERRO!");
-            })
-            // cadastrar nova imagem
-            imagem.mv(__dirname + '/images/' + imagem.name);
-        });
+        // definir o tipo de edição
+        try {
+            // objeto de imagem
+            let imagem = req.files.imagem;
+            // sql
+            let sql = `UPDATE produtos SET nome = '${nome}', valor = ${valor}, imagem = '${imagem.name}' WHERE codigo = '${codigo}'`;
+            // executar comando sql 
+            conexao.query(sql, (erro, resultado) => {
+                if (erro) throw erro;
+                // Remover imagem antiga
+                fs.unlink(__dirname + '/images/' + nomeImagem, (erro_imagem) => {
+                    if (erro_imagem) console.log("Imagem deu ERRO!");
+                })
+                // cadastrar nova imagem
+                imagem.mv(__dirname + '/images/' + imagem.name);
+            });
 
 
-    } catch (erro) {
-        // sql
-        let sql = `UPDATE produtos SET nome = '${nome}', valor = ${valor} WHERE codigo = ${codigo}`;
-        // executar comando sql 
-        conexao.query(sql, (erro, resultado) => {
-            // caso funcione
-            if (erro) throw erro;
-        });
-    }
-    // Finalizar rota
-    res.redirect("/okEdicao");
+        } catch (erro) {
+            // sql
+            let sql = `UPDATE produtos SET nome = '${nome}', valor = ${valor} WHERE codigo = '${codigo}'`;
+            // executar comando sql 
+            conexao.query(sql, (erro, resultado) => {
+                // caso funcione
+                if (erro) throw erro;
+            });
+        }
+        // Finalizar rota
+        res.redirect("/okEdicao");
     }
 }
 
